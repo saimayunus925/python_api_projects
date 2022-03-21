2a1231349e7d58d8d9f9fb69db97f5ff7cc0d3e2
saimayunus925 <saimayunus925@gmail.com>
2022-03-20 20:16:02 -0700

    more stuff

--- bebop.py
+++ bebop.py
@@ -5,6 +5,7 @@ import random # random.choice() for lists
 
 def daily_bebop_quote() -> str:
     # generates a random Bebop quote from our API URL
+    print("----------------- DAILY BEBOP QUOTES -----------------")
     quotes = requests.get('https://animechan.vercel.app/api/quotes/anime?title=cowboy bebop').json()
     quote = random.choice(quotes) # dictionary w/ random quote we want
     return f"{quote['quote']} - {quote['character']}" # returns the quote and who said it
