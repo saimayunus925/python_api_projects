72a288b2e5cef163698e1f00642a3a628ce8a746
saimayunus925 <saimayunus925@gmail.com>
2022-03-20 20:39:28 -0700

    added 'current date' to daily Bebop quotes function

--- bebop.py
+++ bebop.py
@@ -2,10 +2,13 @@
 
 import requests # this is for handling API requests
 import random # random.choice() for lists
+import datetime # this is for the daily Bebop quotes function
 
 def daily_bebop_quote() -> str:
     # generates a random Bebop quote from our API URL
+    todays_date = datetime.date.today() # today's date
     print("----------------- DAILY BEBOP QUOTES -----------------")
+    print(f"Current Date: {todays_date}")
     quotes = requests.get('https://animechan.vercel.app/api/quotes/anime?title=cowboy bebop').json()
     quote = random.choice(quotes) # dictionary w/ random quote we want
     return f"{quote['quote']} - {quote['character']}" # returns the quote and who said it
