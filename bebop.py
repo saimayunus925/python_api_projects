88eeecddcf0b79cdc832e2ef4d6edea3b6c27a56
saimayunus925 <saimayunus925@gmail.com>
2022-03-21 15:20:17 -0700

    more stuff

--- bebop.py
+++ bebop.py
@@ -4,16 +4,18 @@ import requests # this is for handling API requests
 import random # random.choice() for lists
 import datetime # this is for the daily Bebop quotes function
 
-def daily_bebop_quote() -> str:
+def daily_bebop_quote():
     # generates a random Bebop quote from our API URL
     todays_date = datetime.date.today() # today's date
     print("----------------- DAILY BEBOP QUOTES -----------------")
-    print(f"Current Date: {todays_date}")
+    print(f"")
     quotes = requests.get('https://animechan.vercel.app/api/quotes/anime?title=cowboy bebop').json()
     quote = random.choice(quotes) # dictionary w/ random quote we want
-    return f"{quote['quote']} - {quote['character']}" # returns the quote and who said it
+    # returns the quote and who said it, along with the current date
+    print(f"Current Date: {todays_date}")
+    print(f"{quote['quote']}")
+    print(f"- {quote['character']}")
 
 
 if __name__ == "__main__":
-    q = daily_bebop_quote()
-    print(q)
+    daily_bebop_quote()
